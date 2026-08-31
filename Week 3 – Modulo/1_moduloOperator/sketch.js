7289ca0c37c926ee0e35ee112c8ec729702a1f22
jos-515 <jocelyneddy@icloud.com>
2026-08-31 17:40:35 +0800

    modulo tasks

--- Week 3 – Modulo/1_moduloOperator/sketch.js
+++ Week 3 – Modulo/1_moduloOperator/sketch.js
@@ -15,7 +15,24 @@ function draw(){
     noFill();
     rect(xpos, ypos, 20, 20); //draws 20x20px rectangles at (xpos,ypos)
     
-    // update new xpos, ypos every frame
-    xpos = (xpos + 20) % 100; // *EDIT CODE HERE
-    ypos = ypos + 20; // *EDIT CODE HERE
+    // --------- Task 1
+
+    //xpos = (xpos + 20) % 200; 
+    //ypos = (ypos + 20) % 100; 
+
+    // --------- Task 2
+
+    xpos = (xpos + 20) % 200; 
+    ypos = (ypos + 20) % 200; 
+
+    // --------- Task 3
+
+    //xpos = (xpos + 20) % 200; 
+    //ypos = (ypos + 20) % 300; 
+
+    // --------- Task 4
+
+    //xpos = (xpos + 20) % 80; 
+    //ypos = (ypos + 20) % 100; 
+
 }
